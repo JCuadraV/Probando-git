ffca4e8b38cef3917cd77062c0f2de0ef4ae0b07
Javier Cuadra Valdés <50491934+JCuadraV@users.noreply.github.com>
2019-10-16 16:19:25 -0300

    volviendo a edit 3

--- Hola.R
+++ Hola.R
@@ -3,6 +3,3 @@ PROBANDO JC ECO EVO
 Probando desde JCuadraV
 Quiero probar si avisa o no
 sigo con la duda
-enlaces que sirven para resolver problemas
-https://stackoverflow.com/questions/16288176/your-branch-is-ahead-of-origin-master-by-3-commits
-https://stackoverflow.com/questions/11646107/you-have-not-concluded-your-merge-merge-head-exists
